b599278fc4d805a35997b6c6654e8ff679502213
yashkandiyal <126400221+yashkandiyal@users.noreply.github.com>
2023-12-31 14:39:15 +0530

    changes

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -1,16 +1,38 @@
-import React, { useEffect } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import '../App.css'
 function Footer() {
-  
+  const [footerVisible, setFooterVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const isAtBottom = window.innerHeight + window.scrollY >= document.body.scrollHeight;
+
+      setFooterVisible(isAtBottom);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
-    <footer>
-      {/* Your footer content */}
+    <>
+    
+      <footer className={footerVisible ? 'visible' : 'hidden'}>
+     
       <i className="fa-brands fa-instagram insta" />
       <i className="fa-brands fa-github github" />
       <i className="fa-brands fa-twitter twitter" />
       <i className="fa-brands fa-linkedin linkedin" />
     </footer>
+    
+    
+    
+    </>
+    
   );
 }
 
